0f13353a3d504597365f375142179777cba1a3f7
Roman <rom.lviv.ua@gmail.com>
2017-06-23 16:56:05 +0300

    add css and js files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function check(){
+	var checkBox = document.getElementById("checkbox").checked;
+	if (checkBox == true) {
+		console.log("чекбокс чекнутий");
+	} else {
+		console.log("чекбокс не чекнутий");
+	}
+	
+	
+}
